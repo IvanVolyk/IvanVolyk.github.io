5414d9b4adf44e48012d0cea8cb44f28088dcc79
Volyk Ivan <volyk.ivan@gmail.com>
2020-04-14 11:19:40 +0300

    News blog

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+﻿$(function(){
+	$('.slider__inner, .news__slider-inner, .autoplay').slick({
+		nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+		prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+		infinite: false,
+		slidesToScroll: 1,
+  		autoplay: true,
+  		autoplaySpeed: 2000,
+  		dots: true	
+	});
+	$('select').styler();
+
+	$('.header__btn-menu').on('click', function() {
+		$('.menu ul').slideToggle();
+	});
+
+});
